dd82fd5f3b9dacd34f297593dad9fb5930e5e269
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-08-30 10:29:39 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -1,3 +1,4 @@
+# %load q01_load_data_tfidf/build.py
 # Default imports
 
 import pandas as pd
@@ -5,6 +6,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Write your solution here :
 
+def q01_load_data_tfidf(path,max_df=0.95,min_df=2,no_features=1000):
+    variable1=pd.read_csv(path)
+    tf_vect=TfidfVectorizer(max_df=max_df,min_df=min_df,max_features=no_features,stop_words='english')
+    variable2=tf_vect.fit_transform(variable1['talkTitle'])
+    variable3=tf_vect.get_feature_names()
+    return variable1,variable2,variable3
+
+# def q01_load_data_tfidf(path, max_df=0.95, min_df=2, no_features=1000):
+#     dataset = pd.read_csv(path)
+#     tfidf_vectorizer = TfidfVectorizer(max_df=max_df, min_df=min_df, max_features=no_features, stop_words='english')
+#     tfidf = tfidf_vectorizer.fit_transform(dataset['talkTitle'])
+#     tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+#     return dataset, tfidf, tfidf_feature_names
 
 
 
--- q04_topic_modelling/build.py
+++ q04_topic_modelling/build.py
@@ -1,3 +1,4 @@
+# %load q04_topic_modelling/build.py
 # Default imports
 
 import numpy as np
@@ -6,6 +7,25 @@ from greyatomlib.nlp_day_02_project.q03_LDA.build import q03_LDA
 
 
 #  Write your solution here :
+def q04_topic_modelling(path,n_top_words=20):
+    v1,v2=q02_count_vectorizer_for_LDA(path)
+    v3=q03_LDA(path)
+    message=[]
+    for topic_idx, topic in enumerate(v3):
+        message =[v2[i] for i in topic.argsort()[:-n_top_words:-1]]
+    return message
+
+# def q04_topic_modelling(path, n_top_words=20):
+#     topic_word = q03_LDA(path)
+#     topics = []
+#     for i, topic_dist in enumerate(topic_word):
+#         matrix, feature_names = q02_count_vectorizer_for_LDA(path)
+#         topic_words = np.array(feature_names)[np.argsort(topic_dist)][:-n_top_words:-1]
+#         func = ('Topic {}: {}'.format(i, ' '.join(topic_words)))
+#         topics.append(func)
+#     return topics
+
+
 
 
 
